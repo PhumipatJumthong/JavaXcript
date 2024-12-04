e6a7fd895577513c462c8fba2126949f46601acb
PhumipatJumthong <s6606021620171@email.kmutnb.ac.th>
2024-12-04 11:24:06 +0700

    wo

---
+++ CP2/arrowfunctoin.js
@@ -0,0 +1,10 @@
+const add = (a,b) => a + b;
+console.log(add(1,2));
+
+const subteact = (a,b) => {
+    const result = a - b;
+    return result;
+
+
+};
+console.log(subteact(4,2));
---
+++ CP2/null un/arrowf-sampls.js
@@ -0,0 +1,20 @@
+const sum = (firstParam ,secondParam) => {
+    return firstParam + secondParam;
+
+};
+console.log(sum(2,5));
+
+const printHello = () => {
+    console.log("hello");
+
+};
+printHello();
+
+const checWeight = (weight) => {
+    console.log(`Baggage weight : ${weight} kilograms`);
+
+};
+checWeight(25);
+
+const multiply = (a,b) => a*b;
+console.log(multiply(2,30));
